e458cfa5df994027e4f4df3173edaea240d62265
kon <k@smo.la>
2020-06-28 23:09:16 -0700

    increased max filesize, updated styling

--- file_serving_stm/main/file_server.c
+++ file_serving_stm/main/file_server.c
@@ -2,8 +2,8 @@
 
 /* Max size of an individual file. Make sure this
  * value is same as that set in upload_script.html */
-#define MAX_FILE_SIZE (200 * 1024) // 200 KB
-#define MAX_FILE_SIZE_STR "200KB"
+#define MAX_FILE_SIZE (1024 * 1024) // 200 KB
+#define MAX_FILE_SIZE_STR "1MB"
 
 /* Scratch buffer size */
 #define SCRATCH_BUFSIZE 8192
